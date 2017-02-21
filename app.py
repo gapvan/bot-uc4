11a2ed5e4eb9c9843934c028efecdded1474cd09
gapvan <joingap.gap@gmail.com>
2017-02-21 15:48:14 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import json
-import requests
+import requests,os
  
 app = Flask(__name__)
 @app.route('/')
@@ -22,7 +22,7 @@ def callback():
     return '',200  
 def sendText(user, text):
     LINE_API = 'https://api.line.me/v2/bot/message/reply'
-    Authorization = 'Bearer ENTER_ACCESS_TOKEN'
+    Authorization = 'LQINTwiRBOztCajzLlm+v2uTDkiYtOdw790W5gItxLukx1L9InomCUKdMe1NgVB7jqzxOam4uiqLhY2SveEXIEKKKtW4W53u/WLBb65EAwAHHJzFwYEVZ0v76xHwosiSfrAis5Ih6iJnCdy/DlJDXwdB04t89/1O/w1cDnyilFU='
     headers = {
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization':Authorization
@@ -37,4 +37,6 @@ def sendText(user, text):
     r = requests.post(LINE_API, headers=headers, data=data)
 
 if __name__ == '__main__':
-     app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
